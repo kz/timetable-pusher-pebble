c7148b19157bec1d0510e2414aec8a3696a74e30
Kelvin Zhang <github@iamkelv.in>
2015-10-22 17:18:34 +0100

    Add configuration of API key

--- src/app.js
+++ src/app.js
@@ -1,60 +1,54 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Settings = require('settings');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var BASE_URL = 'https://timetablepush.me/';
+var API_BASE_URL = BASE_URL + 'api/v1/';
+var CONFIGURATION_URL = BASE_URL + 'app/config';
+
+/*
+ * App Start
+ */
+
+var viewMain = new UI.Card({
+  title: 'Timetable Pusher',
+  subtitle: 'Loading...'
 });
+viewMain.show();
 
-main.show();
+/*
+ * Configuration
+ */
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+// Listen for configuration start
+Settings.config(
+  { 
+    url: CONFIGURATION_URL,
+    autoSave: true,
+  },
+  function(e) {
+    
+  },
+  function(e) {
+    console.log('Configuration closed');
+  }
+);
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+// Listen for configuration complete
+Settings.config(
+  { url: CONFIGURATION_URL },
+  function(e) {
+    console.log('Configuration closed');
+    Settings.option('apiKey', e.apiKey);
+  }
+);
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+/*
+ * Views
+ */
+
+
+
+/*
+ * Error Handlers
+ */
