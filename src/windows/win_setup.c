4cc50133726be838223f419395fcb7b84b38e865
Kelvin Zhang <github@iamkelv.in>
2016-05-28 19:45:10 +0100

    Finish setup window

--- src/windows/win_setup.c
+++ src/windows/win_setup.c
@@ -5,7 +5,11 @@
 #define DIALOG_CONFIG_WINDOW_MESSAGE  "Set up in the\nPebble app"
 
 static Window *s_setup_window;
-static TextLayer *s_body_layer, *s_title_layer;
+
+#if defined(PBL_RECT)
+static TextLayer *s_title_layer;
+#endif
+static TextLayer *s_body_layer;
 static BitmapLayer *s_icon_layer;
 
 static GBitmap *s_icon_bitmap;
@@ -38,17 +42,19 @@ static void window_load(Window *window) {
     GRect bounds = layer_get_bounds(window_layer);
 
     s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CONFIG_REQUIRED);
-    GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
-
+    GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);    
+    bounds.size.h = PBL_IF_RECT_ELSE(bounds.size.h - 10, bounds.size.h - 40);
+    
     const GEdgeInsets icon_insets = GEdgeInsets(
-        (bounds.size.h - bitmap_bounds.size.h) / 2,
+        (bounds.size.h - bitmap_bounds.size.h) / 2 ,
         (bounds.size.w - bitmap_bounds.size.w) / 2);
     s_icon_layer = bitmap_layer_create(grect_inset(bounds, icon_insets));
     bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
     bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
     layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
 
-    const GEdgeInsets title_insets = {.top = 10};
+    #if defined(PBL_RECT)
+    const GEdgeInsets title_insets = {.top = 5};
     s_title_layer = text_layer_create(grect_inset(bounds, title_insets));
     text_layer_set_text(s_title_layer, DIALOG_CONFIG_WINDOW_APP_NAME);
     text_layer_set_text_color(s_title_layer, PBL_IF_COLOR_ELSE(GColorWhite, GColorBlack));
@@ -56,8 +62,12 @@ static void window_load(Window *window) {
     text_layer_set_text_alignment(s_title_layer, GTextAlignmentCenter);
     text_layer_set_font(s_title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
     layer_add_child(window_layer, text_layer_get_layer(s_title_layer));
+    #endif
 
-    const GEdgeInsets body_insets = {.top = 125, .right = 5, .left = 5};
+    #if defined(PBL_ROUND)
+    bounds.size.h = bounds.size.h + 20;
+    #endif
+    const GEdgeInsets body_insets = {.top = 115, .right = 5, .left = 5};
     s_body_layer = text_layer_create(grect_inset(bounds, body_insets));
     text_layer_set_text(s_body_layer, DIALOG_CONFIG_WINDOW_MESSAGE);
     text_layer_set_text_color(s_body_layer, PBL_IF_COLOR_ELSE(GColorWhite, GColorBlack));
@@ -65,12 +75,14 @@ static void window_load(Window *window) {
     text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text_alignment(s_body_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
+    text_layer_enable_screen_text_flow_and_paging(s_body_layer, 2);
 }
 
 static void window_unload(Window *window) {
+    #if defined(PBL_RECT)
     text_layer_destroy(s_title_layer);
+    #endif
     text_layer_destroy(s_body_layer);
-
     bitmap_layer_destroy(s_icon_layer);
     gbitmap_destroy(s_icon_bitmap);
 }
