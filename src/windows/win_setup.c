88747eb900f8075553423991f2a618ea2aceafee
Kelvin Zhang <github@iamkelv.in>
2016-05-28 18:48:09 +0100

    Add graphical setup window

--- src/windows/win_setup.c
+++ src/windows/win_setup.c
@@ -1,14 +1,21 @@
 #include <pebble.h>
 #include "win_setup.h"
 
+#define DIALOG_CONFIG_WINDOW_APP_NAME "Timetable Pusher"
+#define DIALOG_CONFIG_WINDOW_MESSAGE  "Set up in the\nPebble app"
+
 static Window *s_setup_window;
-static TextLayer *s_title_layer, *s_body_layer;
+static TextLayer *s_body_layer, *s_title_layer;
+static BitmapLayer *s_icon_layer;
+
+static GBitmap *s_icon_bitmap;
 
 static void window_load(Window* window);
 static void window_unload(Window* window);
 
 void win_setup_create(void) {
     s_setup_window = window_create();
+    window_set_background_color(s_setup_window, PBL_IF_COLOR_ELSE(GColorDarkGray, GColorWhite));
     window_set_window_handlers(s_setup_window, (WindowHandlers) {
         .load = window_load,
         .unload = window_unload,
@@ -27,34 +34,43 @@ Window* get_setup_window(void) {
 // -------------------------------------------------------------- //
 
 static void window_load(Window *window) {  
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-  s_title_layer = text_layer_create(bounds);
-  text_layer_set_text(s_title_layer, "Setup required!");
-  text_layer_set_text_alignment(s_title_layer, GTextAlignmentCenter);
-  text_layer_set_font(s_title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_background_color(s_title_layer, GColorBlueMoon);
-  text_layer_set_text_color(s_title_layer, GColorWhite);
-  layer_add_child(window_layer, text_layer_get_layer(s_title_layer));
+    s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CONFIG_REQUIRED);
+    GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
 
-  text_layer_enable_screen_text_flow_and_paging(s_title_layer, 2);
+    const GEdgeInsets icon_insets = GEdgeInsets(
+        (bounds.size.h - bitmap_bounds.size.h) / 2,
+        (bounds.size.w - bitmap_bounds.size.w) / 2);
+    s_icon_layer = bitmap_layer_create(grect_inset(bounds, icon_insets));
+    bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
+    bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
 
-  GSize title_size = text_layer_get_content_size(s_title_layer);
-
-  s_body_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y + title_size.h + 8, bounds.size.w, bounds.size.h));
-  text_layer_set_text(s_body_layer, "Open this app\'s settings page under the Pebble phone app to get started.");
-  text_layer_set_text_alignment(s_body_layer, GTextAlignmentCenter);
+    const GEdgeInsets title_insets = {.top = 10};
+    s_title_layer = text_layer_create(grect_inset(bounds, title_insets));
+    text_layer_set_text(s_title_layer, DIALOG_CONFIG_WINDOW_APP_NAME);
+    text_layer_set_text_color(s_title_layer, PBL_IF_COLOR_ELSE(GColorWhite, GColorBlack));
+    text_layer_set_background_color(s_title_layer, GColorClear);
+    text_layer_set_text_alignment(s_title_layer, GTextAlignmentCenter);
     text_layer_set_font(s_title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_background_color(s_body_layer, GColorPictonBlue);
-  text_layer_set_text_color(s_body_layer, GColorWhite);
-  layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
+    layer_add_child(window_layer, text_layer_get_layer(s_title_layer));
 
-  text_layer_enable_screen_text_flow_and_paging(s_body_layer, 2);
+    const GEdgeInsets body_insets = {.top = 125, .right = 5, .left = 5};
+    s_body_layer = text_layer_create(grect_inset(bounds, body_insets));
+    text_layer_set_text(s_body_layer, DIALOG_CONFIG_WINDOW_MESSAGE);
+    text_layer_set_text_color(s_body_layer, PBL_IF_COLOR_ELSE(GColorWhite, GColorBlack));
+    text_layer_set_background_color(s_body_layer, GColorClear);
+    text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+    text_layer_set_text_alignment(s_body_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
 }
 
 static void window_unload(Window *window) {
-    // Destroy the used layers
-  text_layer_destroy(s_title_layer);
-  text_layer_destroy(s_body_layer);
+    text_layer_destroy(s_title_layer);
+    text_layer_destroy(s_body_layer);
+
+    bitmap_layer_destroy(s_icon_layer);
+    gbitmap_destroy(s_icon_bitmap);
 }
